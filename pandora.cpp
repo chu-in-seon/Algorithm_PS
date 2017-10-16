441ef1540eb5c319b3a6bb4d5d0419b7ba4a8919
chu-in-seon <chis3901@naver.com>
2017-10-16 18:26:10 +0900

    Add files via upload

---
+++ pandora.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+ifstream inp("pandora.inp");
+ofstream out("pandora.out");
+int main() {
+	int Test_case = 0;
+	inp >> Test_case;
+	while (Test_case--) {
+
+	}
+	return 0;
+}
