72d56f8436d7bac29b20858dfe0e2527a809c0c2
Chu Inseon <chis3901@naver.com>
2023-01-12 23:10:17 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <functional>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    sort(numbers.begin(), numbers.end(), greater<int>());
+    return numbers[0] * numbers[1];
+}
