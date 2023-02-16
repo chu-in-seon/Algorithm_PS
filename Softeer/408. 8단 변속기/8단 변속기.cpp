0ba2d0195eed9d1ebb868bb1f77c83720c8679be
Chu Inseon <chis3901@naver.com>
2023-02-16 23:57:41 +0900

    Update 8단 변속기.cpp

--- Softeer/408. 8단 변속기/8단 변속기.cpp
+++ Softeer/408. 8단 변속기/8단 변속기.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
-#include<cstdio>
-#include<string>
+#include <iostream>
+#include <cstdio>
+#include <string>
 
 #define MAX_DCT_LEVEL 8
 #define ASC "ascending"
@@ -9,27 +9,32 @@
 
 using namespace std;
 
-int inpArr[MAX_DCT_LEVEL] = {0, };
-int main(int argc, char** argv)
+int inpArr[MAX_DCT_LEVEL] = {
+    0,
+};
+int main(int argc, char **argv)
 {
-	string inpStr;
-	getline(cin, inpStr);
-    sscanf(inpStr.c_str(), "%d %d %d %d %d %d %d %d", 
-        &inpArr[0], &inpArr[1], &inpArr[2], &inpArr[3],
-        &inpArr[4] ,&inpArr[5], &inpArr[6], &inpArr[7]);
+    string inpStr;
+    getline(cin, inpStr);
+    sscanf(inpStr.c_str(), "%d %d %d %d %d %d %d %d",
+           &inpArr[0], &inpArr[1], &inpArr[2], &inpArr[3],
+           &inpArr[4], &inpArr[5], &inpArr[6], &inpArr[7]);
 
     string result = DES;
     int diff = inpArr[0] - inpArr[1];
-    if( -1 == diff ) {
+    if (-1 == diff)
+    {
         result = ASC;
     }
-    for (int idx = 1; idx < MAX_DCT_LEVEL - 1; idx++) {
-        if (diff != inpArr[idx] - inpArr[idx + 1]) {
+    for (int idx = 1; idx < MAX_DCT_LEVEL - 1; idx++)
+    {
+        if (diff != inpArr[idx] - inpArr[idx + 1])
+        {
             result = MIX;
             break;
         }
     }
 
     cout << result;
-	return 0;
+    return 0;
 }
