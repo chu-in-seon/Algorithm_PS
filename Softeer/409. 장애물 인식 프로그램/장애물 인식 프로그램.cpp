9647bd86d7dadda9b0e34108f3bb644d5706c23e
Chu Inseon <chis3901@naver.com>
2023-02-18 01:23:10 +0900

    Update 장애물 인식 프로그램.cpp

--- Softeer/409. 장애물 인식 프로그램/장애물 인식 프로그램.cpp
+++ Softeer/409. 장애물 인식 프로그램/장애물 인식 프로그램.cpp
@@ -1,63 +1,68 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
-int roadMap[27][27] = {{0, },};
+int roadMap[27][27] = {
+    {
+        0,
+    },
+};
 
-int dfs(int i, int j) {
+int dfs(int i, int j)
+{
     int retVal = 1;
     roadMap[i][j] = 0;
-	if (1 == roadMap[i][j + 1]) {
-		roadMap[i][j + 1] = 0;
-		retVal += dfs(i, j + 1);
-	}
-	if (1 == roadMap[i + 1][j]) {
-		roadMap[i + 1][j] = 0;
-		retVal += dfs(i + 1, j);
-	}
-	if (1 == roadMap[i][j - 1]) {
-		roadMap[i][j - 1] = 0;
-		retVal += dfs(i, j - 1);
-	}
-	if (1 == roadMap[i-1][j]) {
-		roadMap[i - 1][j] = 0;
-		retVal += dfs(i - 1, j);
-	}
+    if (1 == roadMap[i][j + 1]) {
+        roadMap[i][j + 1] = 0;
+        retVal += dfs(i, j + 1);
+    }
+    if (1 == roadMap[i + 1][j]) {
+        roadMap[i + 1][j] = 0;
+        retVal += dfs(i + 1, j);
+    }
+    if (1 == roadMap[i][j - 1]) {
+        roadMap[i][j - 1] = 0;
+        retVal += dfs(i, j - 1);
+    }
+    if (1 == roadMap[i - 1][j]) {
+        roadMap[i - 1][j] = 0;
+        retVal += dfs(i - 1, j);
+    }
 
-	return retVal;
+    return retVal;
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-	int len = 0;
-	cin >> len;
-	for(int i = 1; i <= len; i++) {
-            string tmp;
-            cin >> tmp;
-            for (int j = 1; j <= len; j++) {
-                roadMap[i][j] = tmp[j - 1] - '0';
+    int len = 0;
+    cin >> len;
+    for (int i = 1; i <= len; i++) {
+        string tmp;
+        cin >> tmp;
+        for (int j = 1; j <= len; j++) {
+            roadMap[i][j] = tmp[j - 1] - '0';
+        }
+    }
+
+    vector<int> blocks;
+    for (int i = 1; i <= len; i++) {
+        for (int j = 1; j <= len; j++) {
+            if (0 == roadMap[i][j]) {
+                continue;
+            }
+            int blockCount = dfs(i, j);
+            if (0 != blockCount) {
+                blocks.push_back(blockCount);
             }
         }
-	
-	vector<int> blocks;
-	for (int i = 1; i <= len; i++) {
-		for(int j = 1; j <= len; j++) {
-			if (0 == roadMap[i][j]) {
-				continue;
-			}
-			int blockCount = dfs(i, j);
-			if (0 != blockCount) {
-				blocks.push_back(blockCount);
-			}
-		}
-	}
+    }
 
-	cout << blocks.size() << '\n';
-	sort(blocks.begin(), blocks.end());
-	for (const auto block : blocks) {
-		cout << block << '\n';
-	}
-	return 0;
+    cout << blocks.size() << '\n';
+    sort(blocks.begin(), blocks.end());
+    for (const auto block : blocks) {
+        cout << block << '\n';
+    }
+    return 0;
 }
